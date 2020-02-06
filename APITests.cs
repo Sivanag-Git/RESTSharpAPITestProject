fe013315c04f75771f921746a0bcfff20fbc25bc
Sivanag-Git <sivanagit@gmail.com>
2020-02-06 13:38:02 +0530

    First commit

---
+++ APITests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace RestSharpTestProject
+{
+    [TestFixture]
+    public class APITests
+    {
+        [Test]
+        public void GetWithJsonDeserializer()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var result = deserialize.Deserialize<Dictionary<string, string>>(response);
+            Assert.That(result["author"], Is.EqualTo("typicode"), "Author not correct");
+        }
+
+        [Test]
+        public void PostWithAnonymousBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}/profile", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new { name = "Raj" });
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var result = deserialize.Deserialize<Dictionary<string, string>>(response);
+            Assert.That(result["name"], Is.EqualTo("Raj"), "Profile name not correct");
+        }
+
+        [Test]
+        public void PostWithTypeClassBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Posts() {id="4",title="REST API Testing",author="Nani" });
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var result = deserialize.Deserialize<Dictionary<string, string>>(response);
+            Assert.That(result["author"], Is.EqualTo("Nani"), "Author not correct");
+        }
+
+        [Test]
+        public void PostWithGenericExecute()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Posts() { id = "5", title = "RESTSharp", author = "Swathi" });
+            var response = client.Execute<Posts>(request);
+
+            Assert.That(response.Data.author, Is.EqualTo("Nani"), "Author not correct");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2020289e-4bfd-493c-bca9-498a9178b43d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
